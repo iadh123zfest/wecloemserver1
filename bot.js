63fba68f5ae5456ddb4cfd119ee5434487d54465
iadh123zfest <45603056+iadh123zfest@users.noreply.github.com>
2018-12-29 18:15:52 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js'),
+    bot = new Discord.Client({sisableEveryone: true})
+    console.log("muuuuuuuuute,");
+bot.on('guildMemberAdd', member => {
+const mohamed= member.guild.channels.get("528188989283368971");
+if(!mohamed) return;
+if(mohamed) {
+setTimeout(() => mohamed.send(`**Welcome, To Nautica. :champagne_glass: **`), 4000)        
+}
+});
+
+bot.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
